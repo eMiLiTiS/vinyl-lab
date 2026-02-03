02d1e0d76fd02409ecb566f718c0a84dc2d60abd
Emilio Cócera <cocerae@gmail.com>
2026-02-03 10:07:13 +0100

    chore: remove diag endpoint

--- src/pages/diag.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: text/plain; charset=utf-8');
-echo "PHP: " . PHP_VERSION . PHP_EOL;
-echo "mysqli loaded: " . (extension_loaded('mysqli') ? 'YES' : 'NO') . PHP_EOL;
-echo "mysqli_report exists: " . (function_exists('mysqli_report') ? 'YES' : 'NO') . PHP_EOL;
